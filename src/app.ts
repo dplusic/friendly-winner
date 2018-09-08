be7152a79ac517d5d8c01fe72e11898aa8062703
Dongwoon Son <dplusic@gmail.com>
2018-09-08 16:16:56 +0900

    Refactor app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,27 @@
 import * as express from 'express';
 import * as handler from './handler';
 
-const handleError = (res: express.Response) => (e: Error) => {
-    res.status(500);
-    res.send(e);
-};
+const promiseToResponse = (res: express.Response) => <T>(promise: Promise<T>) => promise
+    .then(result => res.send(result))
+    .catch(e => {
+        res.status(500);
+        res.send(e);
+    })
+
+const getUserFromReq = (req: express.Request) => ({ userId: req.query.userId });
+
+const cb1 = <D>(cb: (user: { userId: string }, req: express.Request) => Promise<D>) => (req: express.Request, res: express.Response) => {
+    const user = getUserFromReq(req);
+    const handlePromise = cb(user, req);
+    promiseToResponse(res)(handlePromise);
+}
 
 const app = express();
 
 app.use(express.json());
 
-app.put('/user', (req, res) =>
-    handler.putUser({ userId: req.query.userId })
-        .then(user => {
-            res.send(user)
-        })
-        .catch(handleError(res))
-);
+app.put('/user', cb1(handler.putUser));
 
-app.get('/map', (req, res) =>
-    handler.getMap({ userId: req.query.userId })
-        .then(maps => {
-            res.send(maps)
-        })
-        .catch(handleError(res))
-)
+app.get('/map', cb1(handler.getMap));
 
 app.listen(3000);
